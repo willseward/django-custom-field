19e6df9abd21d786240e3c806f7189413149e845
David Burke <david@burkesoftware.com>
2013-10-21 17:00:09 -0400

    How many times can I fail at travis ci? Let me count the ways.

---
+++ test_settings.py
@@ -0,0 +1,35 @@
+import os
+PROJECT_DIR = os.path.dirname(__file__)
+
+STATIC_URL = PROJECT_DIR + '/static/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'testdb',
+    }
+}
+
+INSTALLED_APPS = (
+    'custom_field',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'south',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.request',
+)
+
+SITE_ID = 1
+
+ROOT_URLCONF = "test_urls"
