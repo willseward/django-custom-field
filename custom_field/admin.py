b7d932ba4f2ab604e9a9fc89ee20f932600ab4bb
David Burke <dmbst32@gmail.com>
2013-10-28 22:30:28 -0400

    Added a test for the admin view

--- custom_field/admin.py
+++ custom_field/admin.py
@@ -6,4 +6,6 @@ class CustomFieldAdmin(admin.ModelAdmin):
     list_display = ('content_type','name')
     list_filter = ('content_type',)
     search_fields = ('content_type__name','name')
-admin.site.register(CustomField, CustomFieldAdmin)
+import sys
+if not 'test' in sys.argv: # Need to register differently in test
+    admin.site.register(CustomField, CustomFieldAdmin)
--- custom_field/tests.py
+++ custom_field/tests.py
@@ -1,22 +1,40 @@
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 from django.test import TestCase
 from .models import CustomField, CustomFieldValue
+from .custom_field import CustomFieldAdmin
 
 
 class CustomFieldTest(TestCase):
-    def test_validation(self):
+    def setUp(self):
         custom_field_ct = ContentType.objects.get(app_label="custom_field",
                                                   model="customfield")
-        custom_field = CustomField.objects.create(
+        self.custom_field = CustomField.objects.create(
             name="test_field",
             content_type=custom_field_ct,
             field_type="i",
         )
+        user_custom_field_ct = ContentType.objects.get(app_label="auth",
+                                                  model="user")
+        self.user_custom_field = CustomField.objects.create(
+            name="test_user_field",
+            content_type=custom_field_ct,
+            field_type="i",
+            default_value=42,
+        )
+        user = User.objects.create_user('temporary', 'temporary@gmail.com',
+                                        'temporary')
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        self.client.login(username='temporary', password='temporary')
+
+    def test_validation(self):
         custom_value = CustomFieldValue.objects.create(
-            field=custom_field,
+            field=self.custom_field,
             value='5.0',
-            object_id=custom_field.id,
+            object_id=self.custom_field.id,
         )
         custom_value.clean()
         custom_value.save()
@@ -27,3 +45,15 @@ class CustomFieldTest(TestCase):
             self.fail('Was able to save string as custom integer field!')
         except ValidationError:
             pass
+
+    def test_admin(self):
+        from django.contrib import admin
+        class CFAdmin(CustomFieldAdmin):
+            model = CustomField
+        admin.site.register(CustomField, CFAdmin)
+        response = self.client.get('/admin/custom_field/customfield/1/')
+        print response
+        self.assertContains(response, '42', count=2)
+        response = self.client.get('/admin/custom_field/customfield/1/') 
+        # Make sure we aren't adding it on each get
+        self.assertContains(response, '42', count=2)
