be579a03ccbe2d6361ab8e9fd9b4aef052bbe2fa
David Burke <dmbst32@gmail.com>
2015-03-09 15:02:51 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-custom-field",
-    version = "2.5",
+    version = "2.6",
     author = "David Burke",
     author_email = "david@burkesoftware.com",
     description = ("End user custom fields for Django including contrib.admin support"),
